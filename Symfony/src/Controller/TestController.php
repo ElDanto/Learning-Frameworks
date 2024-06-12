ec0f713537c1f69fd9bcdf0594a7ce13c6568d19
ElDanto <pavlovian.ch@gmail.com>
2024-06-12 20:23:46 +0300

    Lesson 2: Routes and controllers

--- Symfony/src/Controller/TestController.php
+++ Symfony/src/Controller/TestController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController
+class TestController extends AbstractController
 {
 //    #[Route(path: '/test/{id}', name: 'test', requirements: ['id' => '\d{2,}'])]
 //    #[Route(path: '/test', name: 'test', condition: "request.headers.get('X-Test')=='foo'")]
     #[Route(path: '/test/{id<\d+>}', name: 'test')]
-    public function index(Request $request) :never
+    public function index(Request $request) :Response
     {
+        return $this->json([
+            'id' => $request->get('id'),
+        ]);
         echo $request->query->get('id');
         die;
     }
